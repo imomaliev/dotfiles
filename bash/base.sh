fc0ebe43f8f71d15d6666be66e5d85f058b0cfc8
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2016-05-10 23:42:19 +0600

    inputrc customization

--- bash/base.sh
+++ bash/base.sh
@@ -17,19 +17,7 @@ hg_branch() {
 }
 
 # prompt customization
-export PS1=" \[\e[1;34m\]\w\[\e[m\] \[\e[1;33m\]\$(hg_branch)\[\e[m\]\\[\e[1;33m\]\$(__git_ps1 '[%s] ')\[\e[m\]\n❯ "
-
-# Use vi mode
-set -o vi
-
-# show all possible compleations
-bind "set show-all-if-ambiguous on"
-
-# tab cycle through files
-## bind "TAB:menu-complete"
-
-# case insensitive completion
-bind "set completion-ignore-case on"
+export PS1=" \[\e[1;34m\]\w\[\e[m\] \[\e[1;33m\]\$(hg_branch)\[\e[m\]\\[\e[1;33m\]\$(__git_ps1 '[%s] ')\[\e[m\]❯ "
 
 # set default editor to nvim
 export EDITOR=$(which nvim)
