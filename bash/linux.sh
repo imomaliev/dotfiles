a56d241492586688bab62c11d9e0dc8f188a820f
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2016-05-19 14:44:13 +0600

    fix less coloring

--- bash/linux.sh
+++ bash/linux.sh
@@ -1,6 +1,6 @@
 source $HOME/Development/dotfiles/bash/base.sh
 
-export TERM=screen-256color
+export TERM=xterm-256color
 
 source $HOME/Development/dotfiles/bash/aliases.sh
 source $HOME/Development/dotfiles/bash/functions.sh
