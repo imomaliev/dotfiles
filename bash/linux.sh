5f4fe49fbdfc37abc64c42c67306ba7b6ab55a18
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2016-06-09 18:42:28 +0600

    disable hpp files in ctags

--- bash/linux.sh
+++ bash/linux.sh
@@ -2,5 +2,8 @@ source $HOME/Development/dotfiles/bash/base.sh
 
 export TERM=xterm-256color
 
+# http://stackoverflow.com/a/791800/3627387
+[[ $- == *i* ]] && stty -ixon
+
 source $HOME/Development/dotfiles/bash/aliases.sh
 source $HOME/Development/dotfiles/bash/functions.sh
