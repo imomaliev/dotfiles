ad2d18ae6e5365df864e45c0c3eb096be6fb5fdf
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2016-12-29 13:18:35 +0700

    added time to prompt

--- bash/base.sh
+++ bash/base.sh
@@ -17,7 +17,7 @@ hg_branch() {
 }
 
 # prompt customization
-export PS1=" \[\e[1;34m\]\w\[\e[m\] \[\e[1;33m\]\$(hg_branch)\[\e[m\]\\[\e[1;33m\]\$(__git_ps1 '[%s] ')\[\e[m\]\n ❯ "
+export PS1=" \[\e[1;34m\]\w\[\e[m\] \[\e[1;33m\]\$(hg_branch)\[\e[m\]\\[\e[1;33m\]\$(__git_ps1 '[%s] ')\[\e[m\]|\[\e[1;32m\]\D{%H:%M:%S %d-%b-%y}\[\e[m\]|\n ❯ "
 
 # set default editor to nvim
 export EDITOR=$(which nvim)
