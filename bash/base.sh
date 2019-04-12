66ff7490cde70588d79f4524df62fb7319eea15f
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2019-04-12 13:23:45 +0700

    show jobs in prompt

--- bash/base.sh
+++ bash/base.sh
@@ -28,11 +28,16 @@ __venv_ps1() {
     fi
 }
 
+__jobs_ps1() {
+    if [ -n "$(jobs -p)" ]; then echo {$(jobs -p | wc -l)}; fi
+}
+
 __bash_prompt_command() {
+    BASE_PROMPT="\$(__hg_ps1)\[\e[m\]\[\e[1;33m\]\$(__git_ps1 '[%s] ')\[\e[m\]\[\e[1;37m\]|\D{%H:%M:%S %d-%b-%y}|\[\e[m\] \[\e[1;35m\]\$(__jobs_ps1)\[\e[m\]\n ❯ "
     if [[ -n "$DIRENV_DIR" ]]; then
-        export PS1=" \[\e[1;32m\]\$(__venv_ps1)\[\e[m\]\[\e[1;33m\]\[\e[1;34m\]\[\e[4m\]\w\[\e[m\]\[\e[m\] \[\e[1;33m\]\$(__hg_ps1)\[\e[m\]\[\e[1;33m\]\$(__git_ps1 '[%s] ')\[\e[m\]\[\e[1;37m\]|\D{%H:%M:%S %d-%b-%y}|\[\e[m\]\n ❯ "
+        export PS1=" \[\e[1;32m\]\$(__venv_ps1)\[\e[m\]\[\e[1;33m\]\[\e[1;34m\]\[\e[4m\]\w\[\e[m\]\[\e[m\] \[\e[1;33m\]$BASE_PROMPT"
     else
-        export PS1=" \[\e[1;32m\]\$(__venv_ps1)\[\e[m\]\[\e[1;33m\]\[\e[1;34m\]\w\[\e[m\] \[\e[1;33m\]\$(__hg_ps1)\[\e[m\]\[\e[1;33m\]\$(__git_ps1 '[%s] ')\[\e[m\]\[\e[1;37m\]|\D{%H:%M:%S %d-%b-%y}|\[\e[m\]\n ❯ "
+        export PS1=" \[\e[1;32m\]\$(__venv_ps1)\[\e[m\]\[\e[1;33m\]\[\e[1;34m\]\w\[\e[m\] \[\e[1;33m\]$BASE_PROMPT"
     fi
 }
 
