e281e3a4a0dd4df309c406f0fa4d27381782be78
Sardorbek Imomaliev <imomaliev@hey.com>
2023-02-25 20:07:49 +0500

    install more treesitter parsers

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -150,7 +150,7 @@ map("i", "<CR>", "<C-G>u<CR>")
 -- :help nvim-treesitter-quickstart
 require("nvim-treesitter.configs").setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "lua", "vim", "help", "python" },
+  ensure_installed = { "lua", "vim", "help", "python", "toml", "terraform", "markdown", "go", "yaml" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
