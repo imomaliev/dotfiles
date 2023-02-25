8d1477b1f9a7aa2c5eb01aa2231f6ea4f080773a
Sardorbek Imomaliev <imomaliev@hey.com>
2023-02-25 16:31:34 +0500

    install and configure lazy.nvim with base plugins for my workflow
    add documentation about vim-differences and default extra mappings

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -1,4 +1,12 @@
 --[[
+NOTE: There are many options enabled by default in comparison to vim
+  :help vim-differences
+
+  Default Plugins
+    "man.lua" plugin is enabled, so `:Man` is available by default.
+    "matchit" plugin is enabled. To disable it in your config: >
+
+
 SUGGESTION: use pre-installed colorscheme 'habamax' instead of default it is much
 more pleasant for the eyes
 
@@ -42,3 +50,54 @@ vim.o.splitbelow = true
 -- a buffer to a file, and restores undo history from the same file on buffer
 -- read.
 vim.o.undofile = true
+
+-- Package Manager
+--
+-- https://github.com/folke/lazy.nvim#-installation
+-- TODO: should I use `stdpath("data")/site` as recommended in `:help runtimepath`?
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.runtimepath:prepend(lazypath)
+
+-- Make sure to set `mapleader` before lazy so your mappings are correct
+--
+-- To define a mapping which uses the "g:mapleader" variable, the special string
+-- "<Leader>" can be used.  It is replaced with the string value of "g:mapleader".
+-- If "g:mapleader" is not set or empty, a backslash is used instead.
+vim.g.mapleader = " "
+
+require("lazy").setup {
+  -- https://github.com/direnv/direnv.vim
+  "direnv/direnv.vim",
+  -- https://github.com/editorconfig/editorconfig-vim
+  "editorconfig/editorconfig-vim",
+  -- https://github.com/junegunn/fzf/blob/master/README-VIM.md#installation
+  { dir = vim.fn.system("brew --prefix fzf"):gsub("\n", "") },
+  -- https://github.com/junegunn/fzf.vim
+  -- TODO: Check if we want to use https://github.com/ibhagwan/fzf-lua instead
+  {
+    "junegunn/fzf.vim",
+    keys = {
+      -- TODO: understand why we are using <C-U> instead of <Cmd>
+      { "<Leader>tt", ":<C-U>Files .<CR>", desc = "FZF Files" },
+      { "<Leader>tl", ":<C-U>Buffers<CR>", desc = "FZF Buffers" },
+      { "<Leader>tr", ":<C-U>History<CR>", desc = "FZF History" },
+      { "<Leader>tp", ":<C-U>Tags<CR>", desc = "FZF Tags" },
+      { "<Leader>tw", ":<C-U>Windows<CR>", desc = "FZF Windows" },
+    },
+  },
+  -- https://github.com/roxma/vim-tmux-clipboard
+  "roxma/vim-tmux-clipboard",
+}
+
+-- XXX: remove this
+vim.cmd.colorscheme "habamax"
