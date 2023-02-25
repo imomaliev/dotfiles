139c73b4b4dc7ea150b69262c83b7c663a95200b
Sardorbek Imomaliev <imomaliev@hey.com>
2023-02-25 19:43:15 +0500

    cleanup treesitter config and add modules

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -147,15 +147,10 @@ map("c", "<C-B>", "<C-A>")
 map("i", "<CR>", "<C-G>u<CR>")
 
 -- [[TreeSitter]]
+-- :help nvim-treesitter-quickstart
 require("nvim-treesitter.configs").setup {
-  -- A directory to install the parsers into.
-  -- If this is excluded or nil parsers are installed
-  -- to either the package dir, or the "site" dir.
-  -- If a custom path is used (not nil) it must be added to the runtimepath.
-  parser_install_dir = "/some/path/to/store/parsers",
-
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust" },
+  ensure_installed = { "lua", "vim", "help", "python" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -163,24 +158,22 @@ require("nvim-treesitter.configs").setup {
   -- Automatically install missing parsers when entering buffer
   auto_install = false,
 
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
-
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
-
-    -- list of language that will be disabled
-    disable = { "c", "rust" },
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+  -- :help nvim-treesitter-highlight-mod
+  highlight = { enable = true },
+  -- :help nvim-treesitter-incremental-selection-mod
+  -- - keymaps:
+  --   - init_selection: in normal mode, start incremental selection.
+  --     Defaults to `gnn`.
+  --   - node_incremental: in visual mode, increment to the upper named parent.
+  --     Defaults to `grn`.
+  --   - scope_incremental: in visual mode, increment to the upper scope
+  --     (as defined in `locals.scm`). Defaults to `grc`.
+  --   - node_decremental: in visual mode, decrement to the previous named node.
+  --     Defaults to `grm`.
+  incremental_selection = { enable = true },
+  -- :help nvim-treesitter-indentation-mod
+  indent = { enable = true },
 }
-vim.opt.runtimepath:append "/some/path/to/store/parsers"
 
 -- XXX: remove this
 vim.cmd.colorscheme "habamax"
