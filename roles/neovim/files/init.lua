038b8f3e5e6b3d4e0fa696fc0c6b9e0d19575d20
Sardorbek Imomaliev <imomaliev@hey.com>
2023-02-25 19:35:30 +0500

    nvim-treesitter quickstart

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -24,6 +24,7 @@ more pleasant for the eyes
 
   :colorscheme habamax
 --]]
+-- [[Options]]
 -- Print the line number in front of each line.
 vim.o.number = true
 
@@ -63,15 +64,13 @@ vim.o.splitbelow = true
 -- read.
 vim.o.undofile = true
 
-
--- Providers
+-- [[Providers]]
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 
-
--- Package Manager
+-- [[Package Manager]]
 --
 -- https://github.com/folke/lazy.nvim#-installation
 -- TODO: should I use `stdpath("data")/site` as recommended in `:help runtimepath`?
@@ -118,9 +117,11 @@ require("lazy").setup {
   -- https://github.com/roxma/vim-tmux-clipboard
   -- TODO: check if we can just use g:clipboard
   "roxma/vim-tmux-clipboard",
+  -- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation#lazynvim
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 }
 
--- Mappings
+-- [[Mappings]]
 --   :help vim.keymap.set()
 
 local map = vim.keymap.set
@@ -143,7 +144,43 @@ map("c", "<C-B>", "<C-A>")
 -- Use CTRL-G u to first break undo, so that you can undo each i_<CR> separately
 --
 -- Begin new line.
-map('i', '<CR>', '<C-G>u<CR>')
+map("i", "<CR>", "<C-G>u<CR>")
+
+-- [[TreeSitter]]
+require("nvim-treesitter.configs").setup {
+  -- A directory to install the parsers into.
+  -- If this is excluded or nil parsers are installed
+  -- to either the package dir, or the "site" dir.
+  -- If a custom path is used (not nil) it must be added to the runtimepath.
+  parser_install_dir = "/some/path/to/store/parsers",
+
+  -- A list of parser names, or "all"
+  ensure_installed = { "c", "lua", "rust" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  auto_install = false,
+
+  -- List of parsers to ignore installing (for "all")
+  ignore_install = { "javascript" },
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- list of language that will be disabled
+    disable = { "c", "rust" },
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+vim.opt.runtimepath:append "/some/path/to/store/parsers"
 
 -- XXX: remove this
 vim.cmd.colorscheme "habamax"
